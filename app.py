ec369875f85876a8c94844f7e1fc6abf9a253e6d
unknown <rishrash2712@gmail.com>
2025-04-27 23:56:19 +0530

    embeddings size compression

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def chat():
     return str(response['answer'])
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 8080, debug = False)
+    app.run(host = "0.0.0.0", port = int(os.environ.get('PORT', 5000)), debug = False)
